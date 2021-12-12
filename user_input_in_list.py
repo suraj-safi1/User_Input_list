7b2c6de1f4a158a7d96f9187aebec347f6f2a2b4
SURAJ SAFI <94732511+suraj-safi1@users.noreply.github.com>
2021-12-12 12:50:21 +0530

    user defined list

---
+++ user_input_in_list.py
@@ -0,0 +1,10 @@
+#input taken from user and insert in list
+f1=input("Enter fruit name 1 \n")
+f2=input("Enter fruit name 2\n")
+f3=input("Enter fruit name 3 \n")
+f4=input("Enter fruit name 4 \n")
+f5=input("Enter fruit name 5 \n")
+f6=input("Enter fruit name 6 \n")
+f7=input("Enter fruit name 7 \n")
+fruits=[f1,f2,f3,f4,f5,f6,f7]
+print("My fav fruits are \n",fruits)
